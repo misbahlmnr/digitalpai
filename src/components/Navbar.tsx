a71120f44a0711a5a1cde2ee521f1dddb1ed6fdb
misbahx <misbahx.id@gmail.com>
2025-07-18 15:08:04 +0700

    feat(navbar): add auth links and cart placeholder

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,10 @@ import Link from "next/link";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import Icons from "./Icons";
 import NavItems from "./NavItems";
+import { buttonVariants } from "./ui/button";
 
 const Navbar = () => {
+  const user = null;
   return (
     <div className="sticky top-0 z-50 bg-white inset-x-0 h-16">
       <header className="relative bg-white">
@@ -21,6 +23,47 @@ const Navbar = () => {
 
               <div className="hidden lg:ml-8 lg:block z-50 lg:self-stretch">
                 <NavItems />
+              </div>
+
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  {!user && (
+                    <Link
+                      href={"/sign-in"}
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Sign in
+                    </Link>
+                  )}
+                  {!user && (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  )}
+                  {user ? (
+                    <p>tes</p>
+                  ) : (
+                    <Link
+                      href={"/sign-up"}
+                      className={buttonVariants({ variant: "ghost" })}
+                    >
+                      Create account
+                    </Link>
+                  )}
+                  {user && (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  )}
+                  {!user && (
+                    <div className="flex lg:ml-6">
+                      <span
+                        className="h-6 w-px bg-gray-200"
+                        aria-hidden="true"
+                      />
+                    </div>
+                  )}
+
+                  <div className="ml-4 flow-root lg:ml-6">
+                    {/* TODO: Cart */}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
